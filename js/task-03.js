0ae6dc45e9f81007664d95606f5da1974470379e
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2024-01-23 17:15:47 +0200

    adjusted t3 design

--- js/task-03.js
+++ js/task-03.js
@@ -41,19 +41,32 @@ Adăugați un stil minim de poziționare a galeriei (flexbox sau grid) prin:
  */
 //Declaration
 const list = document.querySelector(".gallery");
+list.style.listStyleType = "none";
+list.style.display = "flex";
+list.style.flexDirection= "row";
+list.style.justifyContent= "center";
+list.style.gap = "40px";
+list.style.backgroundColor = "bisque";
+list.style.paddingTop = "20px";
+list.style.paddingBottom = "20px";
 const fragment = document.createDocumentFragment();
 
 //Function
 function listing(arr){
                        for( let i = 0; i <arr.length; i++ )
                           {const elemLi = document.createElement("li");
-                            elemLi.classList.add("itemsT3");
-                            const elemLiImg = document.createElement("img")
-                            elemLi.insertAdjacentElement("beforeend", elemLiImg)
-                            elemLiImg.classList.add("imageT3");
-                            elemLiImg.src = arr[i].url;
-                            elemLiImg.alt = arr[i].alt;
-                            fragment.append(elemLi);}
+                           elemLi.classList.add("itemsT3");
+
+                           const elemLiImg = document.createElement("img")
+                           elemLiImg.style.width = "400px";
+                           elemLiImg.style.height = "300px";
+                           elemLiImg.style.borderRadius = "40px";
+
+                           elemLi.insertAdjacentElement("beforeend", elemLiImg)
+                           elemLiImg.classList.add("imageT3");
+                           elemLiImg.src = arr[i].url;
+                           elemLiImg.alt = arr[i].alt;
+                           fragment.append(elemLi);}
                      };
 listing(images);                    
 list.append(fragment);
--- js/task-10.js
+++
@@ -1,6 +0,0 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
-// M-am apucat de task, dar nu l-am terminat...ti le-am trimis pe astea pana il termin si pe asta
